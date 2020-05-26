c30ee4a6a5cd6a1dcc38b247bfdeede3c00d7982
Tobias Klock <tobiasklock84@gmail.com>
2020-05-26 13:53:22 +0200

    add package.json, webpack, typescript, linting and basic html file

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Navbar = () => <h1>Instant Chat Navbar</h1>;
+
+export default Navbar;
---
+++ frontend/webpack.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import webpack, { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+interface Env {
+  development: boolean;
+  production: boolean;
+}
+const webpackConfig = (env: Env): Configuration => ({
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      components: path.resolve(__dirname, "./src/components/"),
+    },
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /dist/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.PRODUCTION": env.production || !env.development,
+      "process.env.VERSION": JSON.stringify(require("./package.json").version),
+    }),
+    new ForkTsCheckerWebpackPlugin({ eslint: true }),
+  ],
+});
+
+export default webpackConfig;
