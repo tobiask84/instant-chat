7182ae93687f998a361fba66903b8ee377db8e66
Tobias Klock <tobiasklock84@gmail.com>
2020-05-27 12:08:52 +0200

    wait for socket io connections

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,13 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req: any, res: any) => {
     res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', () => {
+    console.log('a user connected');
 });
 
 http.listen(3000, () => {
