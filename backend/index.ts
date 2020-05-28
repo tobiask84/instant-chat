ac06f116301301ef660207cf842e602d1bd7542a
Tobias Klock <tobiasklock84@gmail.com>
2020-05-28 15:29:22 +0200

    make ssl possible

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,19 @@
+import * as https from "https";
+const fs = require('fs')
 const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http');
+
+const privateKeyPath = process.env.INSTANT_CHAT_PRIVATE_KEY;
+const fullChainPath = process.env.INSTANT_CHAT_FULL_CHAIN;
+
+const isTls = privateKeyPath && fullChainPath;
+const protocol = isTls ? https : http
+const server = protocol.createServer({
+        key: isTls ? fs.readFileSync(privateKeyPath) : undefined,
+        cert: isTls ? fs.readFileSync(fullChainPath): undefined
+    }, app).listen(3000);
+
+const io = require('socket.io')(server);
 
 app.get('/', (req: any, res: any) => {
     res.send('<h1>Chat Server is running. Websocket is open.</h1>');
@@ -13,6 +26,3 @@ io.on('connection', (socket: any) => {
     });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-});
